30c20a64532ef3a50bd289527e56887dedba3625
r4mercur <bjarne.braeuer@protonmail.com>
2025-11-05 10:06:54 +0100

    extract fallback method

--- search/user.go
+++ search/user.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"io"
 	"log"
+
+	"github.com/elastic/go-elasticsearch/v8"
 )
 
 func FetchRecommendationsFromElastic(userId string) ([]string, error) {
@@ -15,24 +17,11 @@ func FetchRecommendationsFromElastic(userId string) ([]string, error) {
 	// More Like This query with exclusion of the same user
 	query := map[string]interface{}{
 		"query": map[string]interface{}{
-			"bool": map[string]interface{}{
-				"must": []map[string]interface{}{
-					{
-						"more_like_this": map[string]interface{}{
-							"fields":          []string{"hobby", "interest"},
-							"like":            []map[string]interface{}{{"_index": "users", "_id": userId}},
-							"min_term_freq":   1,
-							"max_query_terms": 12,
-						},
-					},
-				},
-				"must_not": []map[string]interface{}{
-					{
-						"term": map[string]interface{}{
-							"id": userId,
-						},
-					},
-				},
+			"more_like_this": map[string]interface{}{
+				"fields":        []string{"hobby", "interest"},
+				"like":          []map[string]interface{}{{"_index": "users", "_id": userId}},
+				"min_term_freq": 1,
+				"min_doc_freq":  1,
 			},
 		},
 		"size": 10,
@@ -84,62 +73,70 @@ func FetchRecommendationsFromElastic(userId string) ([]string, error) {
 
 	// Fallback strategy if no recommendations are found
 	if len(recommendations) == 0 {
-		fallbackQuery := map[string]interface{}{
-			"query": map[string]interface{}{
-				"bool": map[string]interface{}{
-					"must_not": []map[string]interface{}{
-						{
-							"term": map[string]interface{}{
-								"id": userId,
-							},
-						},
-					},
-				},
-			},
-			"size": 10,
-		}
-
-		var fallbackBuf bytes.Buffer
-		if err := json.NewEncoder(&fallbackBuf).Encode(fallbackQuery); err != nil {
-			return nil, err
-		}
-
-		log.Printf("Elasticsearch Fallback Query: %s", fallbackBuf.String())
-
-		fallbackRes, err := esClient.Search(
-			esClient.Search.WithContext(context.Background()),
-			esClient.Search.WithIndex("users"),
-			esClient.Search.WithBody(&fallbackBuf),
-			esClient.Search.WithTrackTotalHits(true),
-		)
-		if err != nil {
-			return nil, err
-		}
-		defer func(Body io.ReadCloser) {
-			err := Body.Close()
-			if err != nil {
-				log.Printf("Error closing response body: %s", err)
-			}
-		}(fallbackRes.Body)
-
-		if fallbackRes.IsError() {
-			return nil, fmt.Errorf("error fetching fallback recommendations: %s", fallbackRes.String())
-		}
-
-		var fallbackResult map[string]interface{}
-		if err := json.NewDecoder(fallbackRes.Body).Decode(&fallbackResult); err != nil {
-			return nil, err
-		}
-
-		fallbackHits := fallbackResult["hits"].(map[string]interface{})["hits"].([]interface{})
-		for _, hit := range fallbackHits {
-			source := hit.(map[string]interface{})["_source"].(map[string]interface{})
-			recommendation := source["id"].(string)
-			if recommendation != userId {
-				recommendations = append(recommendations, recommendation)
-			}
+		_, fallbackRecommendations, err2 := getFallbackRecommendationsFromUserIndex(userId, esClient, recommendations)
+		if err2 != nil {
+			return fallbackRecommendations, err2
 		}
 	}
 
 	return recommendations, nil
+}
+
+func getFallbackRecommendationsFromUserIndex(userId string, esClient *elasticsearch.Client, recommendations []string) ([]string, []string, error) {
+	fallbackQuery := map[string]interface{}{
+		"query": map[string]interface{}{
+			"bool": map[string]interface{}{
+				"must_not": []map[string]interface{}{
+					{
+						"term": map[string]interface{}{
+							"id": userId,
+						},
+					},
+				},
+			},
+		},
+		"size": 10,
+	}
+
+	var fallbackBuf bytes.Buffer
+	if err := json.NewEncoder(&fallbackBuf).Encode(fallbackQuery); err != nil {
+		return nil, nil, err
+	}
+
+	log.Printf("Elasticsearch Fallback Query: %s", fallbackBuf.String())
+
+	fallbackRes, err := esClient.Search(
+		esClient.Search.WithContext(context.Background()),
+		esClient.Search.WithIndex("users"),
+		esClient.Search.WithBody(&fallbackBuf),
+		esClient.Search.WithTrackTotalHits(true),
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Printf("Error closing response body: %s", err)
+		}
+	}(fallbackRes.Body)
+
+	if fallbackRes.IsError() {
+		return nil, nil, fmt.Errorf("error fetching fallback recommendations: %s", fallbackRes.String())
+	}
+
+	var fallbackResult map[string]interface{}
+	if err := json.NewDecoder(fallbackRes.Body).Decode(&fallbackResult); err != nil {
+		return nil, nil, err
+	}
+
+	fallbackHits := fallbackResult["hits"].(map[string]interface{})["hits"].([]interface{})
+	for _, hit := range fallbackHits {
+		source := hit.(map[string]interface{})["_source"].(map[string]interface{})
+		recommendation := source["id"].(string)
+		if recommendation != userId {
+			recommendations = append(recommendations, recommendation)
+		}
+	}
+	return recommendations, nil, nil
 }
